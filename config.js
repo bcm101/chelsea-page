8f8d911304f4ca2f48f0c7575085d4c0550d7daa
Brandon Wetzel <brandon.m.wetzel@gmail.com>
2022-08-20 18:38:42 -0400

    added google form to links

---
+++ config.js
@@ -0,0 +1,6 @@
+// edit these to change the links
+
+urls = {
+    "subscribe": "https://docs.google.com/forms/d/e/1FAIpQLSf5hMv556C2QZpDb3lqcbZug7lizLoVi4tZ3__ERFkfjtYQ9Q/viewform?usp=pp_url&entry.799152888=Subscribe+to+newsletter&entry.869302928=yes,+subscribe.",
+    "volunteer": "https://docs.google.com/forms/d/e/1FAIpQLSf5hMv556C2QZpDb3lqcbZug7lizLoVi4tZ3__ERFkfjtYQ9Q/viewform?usp=pp_url&entry.799152888=Sign+up+for+volunteering&entry.869302928=yes,+subscribe."
+}
