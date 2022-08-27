64e89b8e5087c9aeb794bd3aaea497b78b6a4923
Brandon Wetzel <brandon.m.wetzel@gmail.com>
2022-08-27 16:53:40 -0400

    added socials medias

--- config.js
+++ config.js
@@ -2,5 +2,9 @@
 
 urls = {
     "subscribe": "https://docs.google.com/forms/d/e/1FAIpQLSf5hMv556C2QZpDb3lqcbZug7lizLoVi4tZ3__ERFkfjtYQ9Q/viewform?usp=pp_url&entry.799152888=Subscribe+to+newsletter&entry.869302928=yes,+subscribe.",
-    "volunteer": "https://docs.google.com/forms/d/e/1FAIpQLSf5hMv556C2QZpDb3lqcbZug7lizLoVi4tZ3__ERFkfjtYQ9Q/viewform?usp=pp_url&entry.799152888=Sign+up+for+volunteering&entry.869302928=yes,+subscribe."
+    "volunteer": "https://docs.google.com/forms/d/e/1FAIpQLSf5hMv556C2QZpDb3lqcbZug7lizLoVi4tZ3__ERFkfjtYQ9Q/viewform?usp=pp_url&entry.799152888=Sign+up+for+volunteering&entry.869302928=yes,+subscribe.",
+    "facebook": "https://www.facebook.com/CHSAthleteAlum/",
+    "instagram": "https://www.instagram.com/chsathleticalum/",
+    "twitter": "https://twitter.com/CHSAthleteAlum"
 }
+
